5817b55e4fc768ff69a300d6a70875fff4c17f44
savinnsk <saviodocuments@gmail.com>
2023-11-29 09:07:21 -0300

    chore: add zustand

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
@@ -7,11 +8,16 @@ export default function Home() {
   const [roomName, setRooName] = useState("");
 
   const handlerSendMessage = () => {
-    socket.emit("message", message, roomName);
+    console.log("click");
+    socket.emit("message", { data: message });
   };
 
+  socket.on("message", ({ data }: any) => {
+    console.log("truee", data);
+  });
+
   useEffect(() => {
-    const socket = io("http://localhost:3000");
+    const socket = io("http://localhost:3001");
     setSocket(socket);
   }, []);
 
@@ -22,10 +28,19 @@ export default function Home() {
           bg-slate-50 dark:bg-slate-900 p-8 text-slate-900 dark:text-slate-100
           h-screen flex flex-col items-center text-center
         </h1>
-        <p mt-4>hello hello hello</p>
 
-        <button className="bg-sky-500 dark:bg-sky-400 text-white dark:bg-text-white enabled px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed">
-          Sign in
+        <input
+          type="text"
+          name="message"
+          onChange={(e) => setMessage(e.target.value)}
+          className="bg-slate-50 dark:bg-slate-900 p-8  text-slate-900 dark:text-slate-100 outline outline-offset-2 flex-1  outline-2  rounded px-2 py-1 m-3"
+        ></input>
+
+        <button
+          onClick={handlerSendMessage}
+          className="bg-sky-500 dark:bg-sky-400 text-white dark:bg-text-white enabled px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed"
+        >
+          Enviar
         </button>
       </div>
     </div>
