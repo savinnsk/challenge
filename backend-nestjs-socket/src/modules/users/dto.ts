7ad0ec6d7af23deab75c6f0e3cf96bafd0cc9ca2
savinnsk <saviodocuments@gmail.com>
2023-11-30 20:47:12 -0300

    feat: add user dto with swagger config

--- backend-nestjs-socket/src/modules/users/dto.ts
+++ backend-nestjs-socket/src/modules/users/dto.ts
@@ -1,6 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+
 export abstract class UserCreateDto {
   id?: string;
+
+  @IsString({ message: 'O nome deve ser uma string' })
+  @ApiProperty({
+    example: 'João',
+  })
   name: string;
+
+  @IsString({ message: 'O nome deve ser uma string' })
+  @ApiProperty({
+    example: 'João123',
+  })
   nickname: string;
+
+  @IsString({ message: 'O nome deve ser uma string' })
+  @ApiProperty({
+    example: 'João',
+  })
   password: string;
 }
