58e379a53fb9e50da12cc3ef69de6a6b39b9b7af
savinnsk <saviodocuments@gmail.com>
2023-11-30 20:56:49 -0300

    feat: add create user service

--- backend-nestjs-socket/src/modules/users/dto.ts
+++ backend-nestjs-socket/src/modules/users/dto.ts
@@ -1,8 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsEmail, IsString } from 'class-validator';
 
 export abstract class UserCreateDto {
   id?: string;
+
+  @IsEmail()
+  @ApiProperty({
+    example: 'joao@gmail.com',
+  })
+  email: string;
 
   @IsString({ message: 'O nome deve ser uma string' })
   @ApiProperty({
--- backend-nestjs-socket/src/modules/users/users.service.ts
+++ backend-nestjs-socket/src/modules/users/users.service.ts
@@ -1,7 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { UserCreateDto } from './dto';
+import { PrismaService } from 'src/infra/database/prisma/prisma-service';
 
 @Injectable()
 export class UsersService {
-  async create(data: UserCreateDto) {}
+  constructor(private prisma: PrismaService) {}
+  async create(data: UserCreateDto) {
+    const existUser = await this.prisma.user.findFirst({
+      where: {
+        email: data.email,
+      },
+    });
+    if (existUser) {
+      throw new Error('User already exists');
+    }
+    const user = await this.prisma.user.create({ data });
+
+    return user;
+  }
 }
