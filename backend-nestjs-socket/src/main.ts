d3d36ed4b1c36cd6a5f228773acb07a2bb2b4a7e
savinnsk <saviodocuments@gmail.com>
2023-11-30 20:42:27 -0300

    chore: add swagger

--- backend-nestjs-socket/src/main.ts
+++ backend-nestjs-socket/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('API PLAY FOR A CAUSE CHAT')
+    .setDescription('A REAL CHAT TO PLAY FOR A CAUSE')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3001);
 }
 bootstrap();
