77ea5f6cc84b894a35fe2ac43a9c9cefcc3c588b
savinnsk <saviodocuments@gmail.com>
2023-12-01 07:33:25 -0300

    feat: add login service

---
+++ backend-nestjs-socket/src/modules/authentication/services/login.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class LoginService {
+  constructor(private readonly jwtService: JwtService) {}
+  async execute({ user }: any): Promise<{ accessToken: string }> {
+    const payload = {
+      email: user.email,
+      sub: user.id,
+      nickname: user.nickname,
+    };
+    return {
+      accessToken: this.jwtService.sign(payload),
+    };
+  }
+}
