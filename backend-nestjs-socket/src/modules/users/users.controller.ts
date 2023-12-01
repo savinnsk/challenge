6af5d87f151266a648c8279d15b2fa82acaf5545
savinnsk <saviodocuments@gmail.com>
2023-11-30 21:01:06 -0300

    feat: add user create controller

--- backend-nestjs-socket/src/modules/users/users.controller.ts
+++ backend-nestjs-socket/src/modules/users/users.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { UserCreateDto } from './dto';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post('')
+  @ApiOperation({ summary: 'Create new user' })
+  @ApiCreatedResponse({
+    status: 201,
+    description: 'New user created successfully',
+  })
+  @ApiBadRequestResponse({ status: 400, description: 'User Already exist' })
+  async create(@Body() data: UserCreateDto) {
+    return this.usersService.create(data);
+  }
 }
