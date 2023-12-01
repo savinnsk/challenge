6c38a81b133d015e2e80184fd1033b299eff6d53
savinnsk <saviodocuments@gmail.com>
2023-12-01 08:50:09 -0300

    cofig: add default values to env

--- backend-nestjs-socket/src/config/auth.env.ts
+++ backend-nestjs-socket/src/config/auth.env.ts
@@ -1,6 +1,6 @@
 import 'dotenv';
 
 export default () => ({
-  jwtSecret: process.env.JWT_SECRET,
+  jwtSecret: process.env.JWT_SECRET || 'defaultsecret',
   jwtExpiresIn: process.env.JWT_EXPIRES_IN || '24h',
 });
