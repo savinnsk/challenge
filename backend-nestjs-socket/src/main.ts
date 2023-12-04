91f1990aedad2b3ae6e0a19679c3468e8cf97dfe
savinnsk <saviodocuments@gmail.com>
2023-12-04 10:21:30 -0300

    fix: add address to enable cors

--- backend-nestjs-socket/src/main.ts
+++ backend-nestjs-socket/src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const corsOptions: CorsOptions = {
+    origin: 'http://54.234.157.163:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+  };
+
+  app.enableCors(corsOptions);
 
   const config = new DocumentBuilder()
     .setTitle('API PLAY FOR A CAUSE CHAT')
